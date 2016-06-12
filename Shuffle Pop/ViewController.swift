260f058fffa2c722658e5b6a742f8ee0639e99b0
bshock84 <bshock84@yahoo.com>
2016-06-11 23:45:12 -0500

    Initial Commit

---
+++ Shuffle Pop/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Shuffle Pop
+//
+//  Created by Ben Shockley on 6/11/16.
+//  Copyright © 2016 Ben Shockley. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
